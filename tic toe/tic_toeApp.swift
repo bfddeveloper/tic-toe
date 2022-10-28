7ddf0cd4a741c797a27e1b221898330960ac7d78
bfddeveloper <dicksonbrody@icloud.com>
2022-10-28 09:53:22 -0500

    Initial Commit

---
+++ tic toe/tic_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_toeApp.swift
+//  tic toe
+//
+//  Created by Brody Dickson on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct tic_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
